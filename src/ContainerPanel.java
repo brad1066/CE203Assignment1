f5b33c7cba5bcd4af62c832bba92889b7e026262
Bradley Hastings <brad@pie.co.de>
2021-10-21 17:14:36 +0100

    Various changes
    Set up support for a list of cuboids,
    Finished the drawing of the cuboid

--- src/ContainerPanel.java
+++ src/ContainerPanel.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 // ContainerPanel class for CE203 Assignment to use and modify if needed
 // Date: 12/10/2021
@@ -7,20 +8,34 @@ import java.awt.*;
 
 public class ContainerPanel extends JPanel{
 
-    Cuboid cuboid;   // Cuboid container object instance
+    ArrayList<Cuboid> cuboids;   // Cuboid container object instance
+    Cuboid cur = null;
+    int count = 0;
 
     public ContainerPanel() {
 
         // When you modify the Cuboid container constructor this line will need to be changed
         // The values for the width/height and length of the cuboid are hard coded below
         // These should be input from the application text fields where the user would type them in.
-        cuboid = new Cuboid(1, new int[]{100, 200}, Color.red);
+        cuboids = new ArrayList<>();
     }
 
+    public void addCuboid(int id, int[] dims, Color c) {
+        Cuboid tmp = new Cuboid(id, dims, c);
+        cuboids.add(tmp);
+    }
+
+    public Cuboid getCuboid(int id) {
+        if (id >= count || id < 0) return null;
+        for (Cuboid cuboid : cuboids) {
+            if (cuboid.getID() == id) return cuboid;
+        }
+        return null;
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        cuboid.drawCuboid(g);
+        if (cur != null) cur.draw(g);
     }
 }
--- src/Cuboid.java
+++ src/Cuboid.java
@@ -7,7 +7,7 @@ import java.awt.*;
 public class Cuboid implements Comparable<Cuboid> {
 
     int id;  // Cuboid id should be a six digit non-negative integer with no leading 0s
-    final int[] pos = {25, 25}; // x, y coords of the top left point of the first face of the cuboid;
+    final int[] pos = {25, 150}; // x, y coords of the top left point of the first face of the cuboid;
     int[] dims; // height/width, length of Cuboid
     Color c;
 
@@ -31,7 +31,7 @@ public class Cuboid implements Comparable<Cuboid> {
                 new Point(pos[0], pos[1]),
                 new Point(pos[0] + this.dims[0], pos[1]),
                 new Point(pos[0] + this.dims[0], pos[1] + this.dims[0]),
-                new Point(pos[0], pos[1] + this.dims[1])
+                new Point(pos[0], pos[1] + this.dims[0])
         };
     }
 
@@ -39,25 +39,27 @@ public class Cuboid implements Comparable<Cuboid> {
     // using the example of the first face complete the remainder of the code to produce an
     // output cuboid as shown in the assignment script
     private Point[] getCubeTwoPoints() {
-        int newX = pos[0] + this.dims[0];
-        int newY = pos[1] + this.dims[0];
+        int newX = pos[0] + this.dims[1];
+        int newY = pos[1] + this.dims[1];
         return new Point[]{
-                new Point(pos[0], pos[1]),
-                new Point(pos[0] + this.dims[0], pos[1]),
-                new Point(pos[0] + this.dims[0], pos[1] + this.dims[0]),
-                new Point(pos[0], pos[1] + this.dims[1])
+                new Point(newX, newY),
+                new Point(newX + this.dims[0], newY),
+                new Point(newX + this.dims[0], newY + this.dims[0]),
+                new Point(newX, newY + this.dims[0])
         };
     }
 
     // You will need to modify this method to set the colour of the Cuboid to be drawn
     // you will also need to complete the For loop to draw the lines connecting each face of the cuboid
-    public void drawCuboid(Graphics g) {
+    public void draw(Graphics g) {
 
         g.drawRect(pos[0], pos[1], this.dims[0], this.dims[0]);
         g.drawRect(pos[0] + this.dims[1], pos[1] + this.dims[1], this.dims[0], this.dims[0]);
 
         // to complete..., draw connecting lines by looping through both points arrays and using the graphics drawline() method
-
+        for (int i = 0; i < 4; i++) {
+            g.drawLine(faceOne[i].x, faceOne[i].y, faceTwo[i].x, faceTwo[i].y);
+        }
     }
 
 
@@ -76,8 +78,8 @@ public class Cuboid implements Comparable<Cuboid> {
 
     // outputs a string representation of the cuboid container object which can be used for testing
     public String toString() {
-        return "Cuboid Data:\n\t" +
-                "Face Width/height: " + dims[0] +
+        return "Cuboid Data:" +
+                "\n\tFace Width/height: " + dims[0] +
                 "\n\tLength: " + dims[1] +
                 "\n\tColour: " + c.toString() + "\n";
     }
